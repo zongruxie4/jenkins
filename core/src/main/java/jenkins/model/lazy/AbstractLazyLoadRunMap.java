8813df1869c9c34c0ff52c8707f68577d3c2ab89
Bennett Zhu <39103557+bennettzhu1@users.noreply.github.com>
2026-04-13 09:34:34 -0400

    Add streamLoadedBuilds method to avoid full map copy in getEstimatedDurationCandidates (#26597)
    * Add cap for getLoadedBuilds to avoid full copy for large build histories
    
    * add lazy loadedBuilds() stream to replace arbitrary capped getLoadedBuilds() method
    
    * Update core/src/test/java/jenkins/model/lazy/AbstractLazyLoadRunMapTest.java
    
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>
    
    * Update core/src/test/java/jenkins/model/lazy/AbstractLazyLoadRunMapTest.java
    
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>
    
    * reimplement getLoadedBuilds to wrap core instead copying into new TreeMap
    
    * add streamLoadedBuilds() and use lazy loading in getEstimatedDurationCandidates()
    
    ---------
    
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

--- core/src/main/java/jenkins/model/lazy/AbstractLazyLoadRunMap.java
+++ core/src/main/java/jenkins/model/lazy/AbstractLazyLoadRunMap.java
@@ -43,6 +43,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -50,6 +51,7 @@ import java.util.function.IntPredicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import jenkins.util.MemoryReductionUtil;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.Beta;
@@ -249,14 +251,20 @@ public abstract class AbstractLazyLoadRunMap<R> extends AbstractMap<Integer, R> 
      * Returns a read-only view of records that has already been loaded.
      */
     public SortedMap<Integer, R> getLoadedBuilds() {
-        TreeMap<Integer, BuildReference<R>> res = new TreeMap<>(Comparator.reverseOrder());
-        for (var entry : this.core.entrySet()) {
-            BuildReference<R> buildRef = entry.getValue();
-            if (buildRef.isSet() && !buildRef.isUnloadable()) {
-                res.put(entry.getKey(), buildRef);
-            }
-        }
-        return new BuildReferenceMapAdapter<>(res, buildResolver);
+        TreeMap<Integer, R> res = new TreeMap<>(Comparator.reverseOrder());
+        streamLoadedBuilds().forEach(r -> res.put(getNumberOf(r), r));
+        return res;
+    }
+
+    /**
+     * Returns a lazy stream of build objects, sorted by newest first, skipping GC builds
+     * Unlike {@link #getLoadedBuilds()}, this doesn't require copying the entire map
+     * @since TODO
+     */
+    public Stream<R> streamLoadedBuilds() {
+        return core.values().stream()
+                .map(BuildReference::get)
+                .filter(Objects::nonNull);
     }
 
     /**
